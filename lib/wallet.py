ea7125ddf3b4e5175b08972b9442c63a70c1671f
Brian Hewitt <durendal@durendals-domain.com>
2017-09-28 23:29:18 -0600

    updates

--- lib/wallet.py
+++ lib/wallet.py
@@ -533,7 +533,7 @@ class Abstract_Wallet(PrintError):
         return c, u, x
 
     def get_spendable_coins(self, exclude_frozen = True, domain = None):
-        return self.get_utxos(domain, exclude_frozen=exclide_frozen, mature=True)
+        return self.get_utxos(domain, exclude_frozen=exclude_frozen, mature=True)
 
     def get_utxos(self, domain = None, exclude_frozen = False, mature = False):
         coins = []
